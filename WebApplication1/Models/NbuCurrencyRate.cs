17cc9287a48449d981ea88f1fb15452ee1381f88
anastasiiadatsko <anastasiia.datskooo@gmail.com>
2025-05-15 04:30:07 +0300

    Додано селектор тижнів із датами для графіків валют

---
+++ WebApplication1/Models/NbuCurrencyRate.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication1.Models
+{
+    public class NbuCurrencyRate
+    {
+        [JsonPropertyName("cc")]
+        public string Cc { get; set; }
+
+        [JsonPropertyName("rate")]
+        public decimal Rate { get; set; }
+
+        [JsonPropertyName("exchangedate")]
+        public string Exchangedate { get; set; }
+    }
+}
